ae2c9412137eaf6fcd6f1b5756cd126b52af8574
Klaudia Skóra <klaud.skora@gmail.com>
2019-12-18 11:27:21 +0100

    Add button - ORDER NOW!

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,6 +5,37 @@ import OrderSummary from '../OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = (props) => (
   <Grid>
@@ -16,6 +47,8 @@ const OrderForm = (props) => (
       <Col xs={12}>
         <OrderSummary tripCost={props.tripCost} options={props.options}/>
       </Col>
+      {console.log('joł', props.options)}
+      <Button onClick={() => sendOrder(props.options, props.tripCost)}>Order now!</Button>
     </Row>
   </Grid>
 );

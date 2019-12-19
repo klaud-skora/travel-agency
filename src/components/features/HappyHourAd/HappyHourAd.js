859f47ce94c0890f3ef351aa92e12e1b1d575f1b
Klaudia Skóra <klaud.skora@gmail.com>
2019-12-20 00:00:42 +0100

    Add code for test 1

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import styles from './HappyHourAd.scss';
+
+class HappyHourAd extends React.Component {
+  render () {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default HappyHourAd;
--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import HappyHourAd from './HappyHourAd';
 import { shallow } from 'enzyme';
-import { exportAllDeclaration } from '@babel/types';
 
 describe('Componentr HappyHourAd', () => {
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
-    exportAllDeclaration(component).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });

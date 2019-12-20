7cc9103becd4bcef0ff3b770082c10f222ca7605
Klaudia Skóra <klaud.skora@gmail.com>
2019-12-20 17:02:19 +0100

    Add code for test 2 - check if heading and description render

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,10 +1,14 @@
 import React from 'react';
-//import styles from './HappyHourAd.scss';
+import styles from './HappyHourAd.scss';
 
 class HappyHourAd extends React.Component {
   render () {
     return (
-      <div></div>
+      <div>
+        <h3 className={styles.title}>Title</h3>
+        {/*<div className={styles.countdown}>3, 2, 1 ... !</div>*/}
+        <div className={styles.promoDescription}>It is your time !</div>
+      </div>
     );
   }
 }
--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -2,9 +2,22 @@ import React from 'react';
 import HappyHourAd from './HappyHourAd';
 import { shallow } from 'enzyme';
 
+const select = {
+  title: '.title',
+  countdown: '.countdown',
+  promoDescription: '.promoDescription',
+};
+
+
 describe('Componentr HappyHourAd', () => {
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
+  });
+  it('should render heading and description', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component.exists(select.title)).toEqual(true);
+    //expect(component.exists(select.countdown)).toEqual(true);
+    expect(component.exists(select.promoDescription)).toEqual(true);
   });
 });
